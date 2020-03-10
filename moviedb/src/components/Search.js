c4a3a47bd6e9609cbabbca23c4a42f7502c93461
root <hish.b92@gmail.com>
2020-03-10 09:51:40 +0000

    Display movie with image and title

--- moviedb/src/components/Search.js
+++ moviedb/src/components/Search.js
@@ -1,10 +1,16 @@
 import React from 'react'
 
 
-function Search () {
+function Search ({ handleInput, search }) {
   return (
     <section className="searchbox-wrap">
-        <input type="text" placeholder="Search for a movie..." className="searchbox" />
+        <input
+          type="text"
+          placeholder="Search for a movie..."
+          className="searchbox" 
+          onChange={handleInput}
+          onKeyPress={search}
+         />
     </section>
   )
 }
